e9cd8c90239ff6e32c248be6f7f48e2d78263405
Ryan <Ryan@CROCODILE>
2018-09-12 13:04:59 -0400

    Add project files.

---
+++ AnotherGo/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace AnotherGo.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
