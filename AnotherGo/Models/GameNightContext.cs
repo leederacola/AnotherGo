2d189b7f4606fe02fe2c887639e8ae520a2963a9
Ryan <Ryan@CROCODILE>
2018-09-12 13:38:00 -0400

    context, connection string added

---
+++ AnotherGo/Models/GameNightContext.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AnotherGo.Models
+{
+    public class GameNightContext : DbContext
+    {
+        public GameNightContext()
+        {
+        }
+
+        public GameNightContext(DbContextOptions<GameNightContext> options)
+            : base(options)
+        {
+        }
+        public virtual DbSet<EventGames> EventGames { get; set; }
+        public virtual DbSet<EventPlayers> EventPlayers { get; set; }
+        public virtual DbSet<Game> Game { get; set; }
+        public virtual DbSet<GameNight> GameNight { get; set; }
+        public virtual DbSet<Library> Library { get; set; }
+        public virtual DbSet<Player> Player { get; set; }
+    }
+}
